6fd8c8f68e009937a957d5f6d96650f92317f709
AvinandanRoy <abhinandanroylipi@gmail.com>
2024-11-11 10:50:18 +0600

    first commit

---
+++ firstReactApp/src/App.jsx
@@ -0,0 +1,14 @@
+import KgButton from "./KgButton"
+
+
+function App() {
+
+  return (
+    <>
+      <h1>I am Avinandan Roy. I started this course since 11.11.2024</h1>
+      <KgButton />
+    </>
+  )
+}
+
+export default App
